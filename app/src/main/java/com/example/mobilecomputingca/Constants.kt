db992dabdad0a24ba34a168372abcef5bd9b5bbc
Paul Doyle <71635533+N00193346@users.noreply.github.com>
2021-11-14 21:56:49 +0000

    Implemented dao

--- app/src/main/java/com/example/mobilecomputingca/Constants.kt
+++ app/src/main/java/com/example/mobilecomputingca/Constants.kt
@@ -2,3 +2,5 @@ package com.example.mobilecomputingca
 
 const val NEW_NOTE_ID = 0
 const val TAG = "noteLogging"
+
+const val NEW_FILM_ID = 0
---
+++ app/src/main/java/com/example/mobilecomputingca/data/NoteDao.kt
@@ -0,0 +1,26 @@
+package com.example.mobilecomputingca.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertNote(note: NoteEntity)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertAll(note: List<NoteEntity>)
+
+    @Query("SELECT * FROM notes ORDER BY date ASC")
+    fun getAll(): LiveData<List<NoteEntity>>
+
+    @Query("SELECT * FROM notes WHERE id = :id")
+    fun getNoteById(id: Int): NoteEntity?
+
+    @Query("SELECT COUNT(*) FROM notes")
+    fun getCount(): Int
+}
