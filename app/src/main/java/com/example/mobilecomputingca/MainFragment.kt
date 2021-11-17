889e48c552b40037a3b9f11279696d758bad76c9
Paul Doyle <71635533+N00193346@users.noreply.github.com>
2021-11-17 20:54:58 +0000

    Added a recycler view

--- app/src/main/java/com/example/mobilecomputingca/MainFragment.kt
+++ app/src/main/java/com/example/mobilecomputingca/MainFragment.kt
@@ -6,18 +6,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mobilecomputingca.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: MainFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = MainFragmentBinding.inflate(inflater, container, false)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        return inflater.inflate(R.layout.main_fragment, container, false)
+
+        //Setting up recyclerview properties
+        with(binding.recyclerView) {
+            //Fixed size so every row has same height
+            setHasFixedSize(true)
+            //Creating divider to put between rows
+            val divider = DividerItemDecoration(
+                context, LinearLayoutManager(context).orientation
+            )
+            //Applying the divider
+            addItemDecoration(divider)
+        }
+
+        return binding.root
+
     }
 
 
