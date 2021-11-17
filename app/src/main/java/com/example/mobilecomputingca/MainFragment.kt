9519b34533780a48cd5bbbc5cd5165bf530bd1e4
Paul Doyle <71635533+N00193346@users.noreply.github.com>
2021-11-17 20:29:58 +0000

    Starting over

--- app/src/main/java/com/example/mobilecomputingca/MainFragment.kt
+++ app/src/main/java/com/example/mobilecomputingca/MainFragment.kt
@@ -2,140 +2,24 @@ package com.example.mobilecomputingca
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
-import android.view.*
 import androidx.fragment.app.Fragment
-import androidx.activity.OnBackPressedCallback
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.mobilecomputingca.data.NoteEntity
-import com.example.mobilecomputingca.databinding.MainFragmentBinding
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
-class MainFragment : Fragment(),
-    NotesListAdapter.ListItemListener{
+class MainFragment : Fragment() {
 
 
     private lateinit var viewModel: MainViewModel
-    private lateinit var binding: MainFragmentBinding
-    private lateinit var adapter: NotesListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-
-        (activity as AppCompatActivity)
-            .supportActionBar?.setDisplayHomeAsUpEnabled(false)
-        setHasOptionsMenu(true)
-
-        requireActivity().title = getString(R.string.app_name)
-
-        binding  = MainFragmentBinding.inflate(inflater, container, false)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-
-        //If the user goes back using phone's back button, move back up to previous screen
-        requireActivity().onBackPressedDispatcher.addCallback(
-            viewLifecycleOwner,
-            object : OnBackPressedCallback(true) {
-                override fun handleOnBackPressed() {
-                    findNavController().navigateUp()
-                }
-            }
-        )
-
-        with(binding.recyclerView) {
-            setHasFixedSize(true)
-            val divider = DividerItemDecoration(
-                context, LinearLayoutManager(context).orientation
-            )
-            addItemDecoration(divider)
-        }
-
-        viewModel.notesList?.observe(viewLifecycleOwner, Observer {
-            Log.i("noteLogging", it.toString())
-            adapter = NotesListAdapter(it, this@MainFragment)
-            binding.recyclerView.adapter = adapter
-            binding.recyclerView.layoutManager = LinearLayoutManager(activity)
-
-            val selectedNotes = savedInstanceState?.getParcelableArrayList<NoteEntity>(SELECTED_NOTES_KEY)
-            adapter.selectedNotes.addAll(selectedNotes ?: emptyList())
-        })
-
-        binding.floatingActionButton.setOnClickListener{
-            editNote(NEW_NOTE_ID)
-        }
-
-        return binding.root
+        return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        val menuId =
-            if (this::adapter.isInitialized &&
-                    adapter.selectedNotes.isNotEmpty()) {
-                R.menu.menu_main_selected_items
-            } else {
-                R.menu.menu_main
-            }
-        inflater.inflate(menuId, menu)
-        super.onCreateOptionsMenu(menu, inflater)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId) {
-            R.id.action_sample_data -> addSampleData()
-            R.id.action_delete -> deleteSelectedNotes()
-            R.id.action_delete_all -> deleteAllNotes()
-            else -> super.onOptionsItemSelected(item)
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    private fun deleteAllNotes(): Boolean {
-        viewModel.deleteAllNotes()
-        return true
-
-    }
-
-    private fun deleteSelectedNotes(): Boolean {
-        viewModel.deleteNotes(adapter.selectedNotes)
-        //Want to clear the selection after a delete
-        Handler(Looper.getMainLooper()).postDelayed({
-            adapter.selectedNotes.clear()
-            requireActivity().invalidateOptionsMenu()
-        }, 100)
-        return true
-    }
-
-    private fun addSampleData(): Boolean {
-            viewModel.addSampleData()
-        return true
-    }
-
-    override fun editNote(noteId: Int) {
-        Log.i(TAG, "onItemClick: received note id $noteId")
-        val action = MainFragmentDirections.actionEditNote(noteId)
-        findNavController().navigate(action)
-    }
-
-    override fun onItemSelectionChanged() {
-        requireActivity().invalidateOptionsMenu()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        if (this::adapter.isInitialized) {
-            outState.putParcelableArrayList(
-                SELECTED_NOTES_KEY,
-                adapter.selectedNotes
-            )
-    }
-        super.onSaveInstanceState(outState)
-    }
 
 
 }
--- app/src/main/java/com/example/mobilecomputingca/MainViewModel.kt
+++ app/src/main/java/com/example/mobilecomputingca/MainViewModel.kt
@@ -1,45 +1,7 @@
 package com.example.mobilecomputingca
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.mobilecomputingca.data.AppDatabase
-import com.example.mobilecomputingca.data.NoteEntity
-import com.example.mobilecomputingca.data.SampleDataProvider
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
-class MainViewModel(app: Application) : AndroidViewModel(app) {
-
-    private val database = AppDatabase.getInstance(app)
-    val notesList = database?.noteDao()?.getAll()
-
-    fun addSampleData() {
-        viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                val sampleNotes = SampleDataProvider.getNotes()
-                database?.noteDao()?.insertAll(sampleNotes)
-            }
-        }
-    }
-
-    fun deleteNotes(selectedNotes: List<NoteEntity>) {
-        viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                val sampleNotes = SampleDataProvider.getNotes()
-                database?.noteDao()?.deleteNotes(selectedNotes)
-            }
-        }
-    }
-
-    fun deleteAllNotes() {
-        viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                database?.noteDao()?.deleteAll()
-            }
-        }
-    }
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
 }
